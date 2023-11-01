4675183572e4adca3ed07cae67ac49a4b0826b35
shouryashashank <shouryashashank@gmail.com>
2023-11-01 20:27:05 +0530

    build fix 2

--- setup.py
+++ setup.py
@@ -22,7 +22,15 @@ setup(
         "Programming Language :: Python :: 3.10",
         "Operating System :: OS Independent",
     ],
-    install_requires=(HERE/"build_requirements.txt").read_text().splitlines(),
+    install_requires=["bson >= 0.5.10",
+                    "pandas >= 2.1.1",
+                    "numpy >= 1.24.1",
+                    "regex >= 2021.4.4",
+                    "PyPDF2 >= 3.0.1",
+                    "docx >= 0.2.4",
+                    "python-docx >= 1.0.1",
+                    "transformers >= 4.29.1"]
+,
     extras_require={
         "dev": ["pytest>=7.0", "twine>=4.0.2"],
     },
