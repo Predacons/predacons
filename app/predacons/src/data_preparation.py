da0693f0559fe70916bc42cc94e6fb972a23b4a2
Shourya Shashank <shouryashashank@gmail.com>
2024-02-23 20:46:12 +0530

    added  : ngenerate_text_data_source_ll , renamed : ngenerate_text_data_source_openai
    added : ngenerate_text_data_source_ll
    renamed : ngenerate_text_data_source_openai

--- app/predacons/src/data_preparation.py
+++ app/predacons/src/data_preparation.py
@@ -1,5 +1,6 @@
 import openai
 import pandas as pd
+from .generate import Generate
 
 class DataPreparation:
     def __generate_data_source_openai(client,gpt_model,prompt, existing_data, temperature=.5):
@@ -24,9 +25,18 @@ class DataPreparation:
             messages=messages,
         )
         return(completion.choices[0].message.content)
-
-    def __generate_text_data_source(client,gpt_model,prompt,number_of_examples,temperature =0.5):
+    
+    def __generate_data_source_llm(model_path, sequence, max_length,existing_data,trust_remote_code = False):
+        messages=f"system : you are assistant and you are generating data which will be used to train a machine learning model.\n\nYou will be given a high-level description of the model we want to train, and from that, you will generate data samples, each with a prompt/response pair.\n\nYou will do so in this format:\n```\nprompt\n-----------\n$prompt_goes_here\n-----------\n\nresponse\n-----------\n$response_goes_here\n-----------\n```\n\nOnly one prompt/response pair should be generated per turn.\n\nFor each turn, make the example slightly more complex than the last, while ensuring diversity.\n\nMake sure your samples are unique and diverse, yet high-quality and complex enough to train a well-performing model.\n\nHere is the type of model we want to train:\n`{sequence}`"
+            
+        if len(existing_data) > 0:
+            if len(existing_data) > 10:
+                existing_data = random.sample(existing_data, 10)
+            for example in existing_data:
+                messages = messages + f" \n assistant : {example}"
+        return Generate.generate_text(model_path, messages, max_length ,trust_remote_code = trust_remote_code)
         
+    def __generate_text_data_source_openai(client,gpt_model,prompt,number_of_examples,temperature =0.5):
         prev_examples = []
         print(f'Generating data source for prompt: {prompt}')
         for i in range(int(number_of_examples)):
@@ -36,5 +46,14 @@ class DataPreparation:
         training_data = "\n \n \n".join(prev_examples)
         return training_data
     
-    def generate_text_data_source(client,gpt_model,prompt,number_of_examples,temperature =0.5):
-        return DataPreparation.__generate_text_data_source(client,gpt_model,prompt,number_of_examples,temperature)
+    def __generate_text_data_source_llm(model_path, sequence, max_length,number_of_examples,trust_remote_code=False):
+        prev_examples = []
+        print(f'Generating data source for prompt: {sequence}')
+        for i in range(int(number_of_examples)):
+            example=DataPreparation.__generate_data_source_llm(model_path, sequence, max_length,prev_examples,trust_remote_code = trust_remote_code)
+    
+    def generate_text_data_source_openai(client,gpt_model,prompt,number_of_examples,temperature =0.5):
+        return DataPreparation.__generate_text_data_source_openai(client,gpt_model,prompt,number_of_examples,temperature)
+    
+    def generate_text_data_source_llm(model_path, sequence, max_length,number_of_examples,trust_remote_code=False):
+        return DataPreparation.__generate_text_data_source_llm(model_path, sequence, max_length,number_of_examples,trust_remote_code=trust_remote_code)
--- app/predacons/src/predacons.py
+++ app/predacons/src/predacons.py
@@ -5,6 +5,7 @@ from .data_preparation import DataPreparation
 
 def rollout():
     print("Predacons rollout !!!")
+    print("Predacons Version: v0.0.110")
     print("\nread_documents_from_directory -- Load data from directory")
     print("    directory -- Directory path")
     print("\nread_multiple_files -- Load data from multiple files")
@@ -39,12 +40,18 @@ def rollout():
     print("    sequence -- Sequence")
     print("    max_length -- Max length")
     print("    trust_remote_code -- Trust remote code")
-    print("\ngenerate_text_data_source -- Generate text data source")
+    print("\ngenerate_text_data_source_openai -- Generate text data source using openai")
     print("    client -- openai client")
     print("    gpt_model -- GPT model used for generation")
     print("    prompt -- Prompt to generate data source")
     print("    number_of_examples -- Number of examples")
     print("    temperature -- Temperature (default 0.5)")
+    print("\ngenerate_text_data_source_ll -- Generate text data source using local or hugging face llm")
+    print("    model_path -- Model path or hugging face model name")
+    print("    sequence -- Sequence (prompt)")
+    print("    max_length -- Max length of the generated text")
+    print("    number_of_examples -- Number of examples")
+    print("    trust_remote_code -- Trust remote code")
     print("\nPredacons rollout !!!")
 
 # Load data
@@ -100,6 +107,11 @@ def generate_output(model_path, sequence, max_length,trust_remote_code = False):
     return Generate.generate_output(model_path, sequence, max_length,trust_remote_code = trust_remote_code)
 
 # Data preparation
-def generate_text_data_source(client,gpt_model,prompt,number_of_examples,temperature =0.5):
-    return DataPreparation.generate_text_data_source(client,gpt_model,prompt,number_of_examples,temperature)
+def generate_text_data_source_openai(client,gpt_model,prompt,number_of_examples,temperature =0.5):
+    return DataPreparation.generate_text_data_source_openai(client,gpt_model,prompt,number_of_examples,temperature)
+
+def generate_text_data_source_llm(model_path, sequence, max_length,number_of_examples,trust_remote_code=False):
+    return DataPreparation.generate_text_data_source_llm(model_path, sequence, max_length,number_of_examples,trust_remote_code=trust_remote_code)
+
+ 
 
