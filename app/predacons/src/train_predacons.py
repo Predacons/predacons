8cca3ed5ce60bdea3c252b034cfc60f06000a5ec
Shourya Shashank <shouryashashank@gmail.com>
2024-07-07 14:29:00 +0530

    updated readme , final commit

--- app/predacons/src/train_predacons.py
+++ app/predacons/src/train_predacons.py
@@ -56,9 +56,10 @@ class TrainPredacons:
     def __trainer(*args,**kwargs):
 
         # use_legacy_trainer
+        print("Initiating predacons training...")
         use_legacy_trainer = kwargs.get('use_legacy_trainer', False)
         if(use_legacy_trainer):
-            print("using lagacy trainer will be removed in further release.")
+            print("Using lagacy trainer will be removed in further release.")
             tokenizer = AutoTokenizer.from_pretrained(kwargs.get('model_name', None))
             train_dataset = TrainPredacons.__load_dataset(kwargs.get('train_file_path', None), kwargs.get('tokenizer', None))
             data_collator = TrainPredacons.__load_data_collator(kwargs.get('tokenizer', None))
@@ -88,7 +89,7 @@ class TrainPredacons:
             return trainer
 
         # load model and tokenizer
-        print("loading model and tokenizer new approach")
+        print("Loading model and tokenizer...")
         model_name = ""
         model = None
         tokenizer = None
@@ -97,32 +98,32 @@ class TrainPredacons:
             quantization_config = None
             if 'quantization_config' in kwargs:
                 quantization_config = kwargs['quantization_config']
-                print("using user provided quantization_config")
+                print("Using user provided quantization_config...")
             auto_quantize = kwargs.get('auto_quantize', "")
 
             if auto_quantize.lower() == "4bit" or auto_quantize.lower() == "high":
-                print("using 4bit quantization")
+                print("Using 4bit quantization...")
                 quantization_config = BitsAndBytesConfig(
                     load_in_4bit=True,
                     bnb_4bit_quant_type="nf4",
                     bnb_4bit_compute_dtype=torch.bfloat16
                 )
             elif  auto_quantize.lower() == "8bit" or auto_quantize.lower() == "low":
-                print("using 8bit quantization")
+                print("Using 8bit quantization...")
                 quantization_config = BitsAndBytesConfig(
                     load_in_8bit =True,
                     bnb_4bit_compute_dtype=torch.bfloat16
                 )
             else:
-                print("either give 4bit/high or 8bit/low as the value of auto_quantize. movig ahead without any quantization" )
+                print("Error: Either give 4bit/high or 8bit/low as the value of auto_quantize. moving ahead without any quantization" )
             tokenizer = AutoTokenizer.from_pretrained(model_name)
             trust_remote_code = kwargs.get('trust_remote_code', False)
 
             model = AutoModelForCausalLM.from_pretrained(model_name,quantization_config = quantization_config,trust_remote_code =trust_remote_code)
-            print(f"model created")
+            print(f"Model ready: {model_name} m")
 
         elif ('model' in kwargs and 'tokenizer' in kwargs):
-            print("using provided model and tokenizer.")
+            print("Using provided model and tokenizer...")
             try:
                 model = kwargs['model']
                 tokenizer = kwargs['tokenizer']
@@ -138,7 +139,7 @@ class TrainPredacons:
             tokenizer.save_pretrained(output_dir)
             model.save_pretrained(output_dir)
         else:
-            print("output directory not provided sving everything to root dir, better stop execution and provide value in output_dir. " )
+            print("Warning: Output directory not provided saving everything to root dir, better stop execution and provide value in output_dir... " )
 
         # load peft_config
         peft_config = None
@@ -155,7 +156,7 @@ class TrainPredacons:
         print(f"peft_config: {peft_config}")
 
         # load training_args
-        print("loading training_args")
+        print("Loading training_args...")
         training_args = kwargs.get('training_args',None)
         if training_args == None:
             per_device_train_batch_size = kwargs.get('per_device_train_batch_size',None)
@@ -171,41 +172,35 @@ class TrainPredacons:
                 )
 
         # dataset configuration
-        # todo : add proper data set configuration with custom function
-        print(f"!!!!creating daa set!!!!")
+        print(f"Creating data set...")
         train_file_type = kwargs.get('train_file_type',None)
         train_file_path = kwargs.get('train_file_path',None)
         train_dataset = kwargs.get('train_dataset',None)
         preprcess_function = kwargs.get('preprcess_function',None)
         if train_file_path != None and train_dataset == None:
-            print(f"train_file_path: {train_file_path}")
-            print(f"train_file_type: {train_file_type}")
-            print(f"!!!!in file path approach")
             if train_file_type not in ("text","csv","json"):
-                print("file type not supported use text,csv or json trying to process as text")
+                print("Error: File type not supported use text,csv or json trying to process as text")
                 train_file_type = "text"
             train_dataset = load_dataset(train_file_type, data_files={"train": train_file_path})
             if train_file_type == "text":
                 train_dataset = train_dataset.map(lambda samples: tokenizer(samples["text"]), batched=True)
             else:
-                print(f"!!!in non text approach!!!")
                 if preprcess_function == None:
-                    print("no preprcess_function provided using default preprcess_function")
+                    print("No preprcess_function provided using default preprcess_function")
                     first_feature = TrainPredacons.get_feature_list(train_dataset)[0]
-                    print(f"using first_feature: {first_feature}, pass preprcess_function to use a custom preprocess_function")
+                    print(f"Using first_feature: {first_feature}, pass preprcess_function to use a custom preprocess_function")
                     train_dataset = train_dataset.map(lambda samples: tokenizer(samples[first_feature]), batched=True)
                 else:
                     train_dataset = train_dataset.map(preprcess_function, batched=True)
         else:
-            print("please provide train_file_path to load dataset other sources will be added in next release")
+            print("Please provide train_file_path to load dataset other sources will be added in next release")
         print(f"train_dataset: {train_dataset}")
         try:
-            print("using custom formatting function")
+            print("Using custom formatting function...")
             formatting_func = TrainPredacons.create_formatting_func(train_dataset)
         except:
-            print("using default formatting function")
+            print("Using default formatting function...")
             formatting_func = TrainPredacons.formatting_func
-        print(f"formatting_func: {formatting_func}")
         trainer = SFTTrainer(
             model=model,
             train_dataset=train_dataset["train"],
@@ -223,7 +218,7 @@ class TrainPredacons:
             checkpoint_folder_exists = any(folder.startswith('checkpoint') for folder in os.listdir(kwargs.get('output_dir', "")))
             trainer.train(resume_from_checkpoint = (checkpoint_folder_exists and kwargs.get('resume_from_checkpoint', "")))
         except:
-            print('Failed to resume from checkpoint. training from scratch.')
+            print('Error: Failed to resume from checkpoint. training from scratch.')
             trainer.train()
         trainer.save_model()
 
