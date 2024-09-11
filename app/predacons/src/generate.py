7e1a73c20143751d4d7a6e42d7593715e4141205
Shourya Shashank <shouryashashank@gmail.com>
2024-09-11 21:25:23 +0530

    added default chat template

--- app/predacons/src/generate.py
+++ app/predacons/src/generate.py
@@ -31,6 +31,25 @@ class Generate:
     def __generate_chat_output(model_path, sequence, max_length,temprature = 0.1,trust_remote_code=False):
         model = Generate.__load_model(model_path,trust_remote_code=trust_remote_code)
         tokenizer = Generate.__load_tokenizer(model_path)
+        if tokenizer.chat_template is None:
+            print("Warning: Chat template not found in tokenizer. Appling default chat template")
+            tokenizer.chat_template = """
+                {{ bos_token }}
+                {% for message in messages %}
+                    {% if message['role'] == 'system' %}
+                        {{ '<start_of_turn>system\n' + message['content'] | trim + '<end_of_turn>\n' }}
+                    {% elif message['role'] == 'user' %}
+                        {{ '<start_of_turn>user\n' + message['content'] | trim + '<end_of_turn>\n' }}
+                    {% elif message['role'] == 'assistant' %}
+                        {{ '<start_of_turn>assistant\n' + message['content'] | trim + '<end_of_turn>\n' }}
+                    {% else %}
+                        {{ raise_exception('Unsupported role: ' + message['role']) }}
+                    {% endif %}
+                {% endfor %}
+                {% if add_generation_prompt %}
+                    {{ '<start_of_turn>assistant\n' }}
+                {% endif %}
+                """
         formatted_chat = tokenizer.apply_chat_template(sequence, tokenize=False, add_generation_prompt=True)
         inputs = tokenizer(formatted_chat, return_tensors="pt", add_special_tokens=False)
         inputs = {key: tensor.to(model.device) for key, tensor in inputs.items()}
@@ -60,6 +79,25 @@ class Generate:
     
     def __generate_chat_output_from_model(model, tokenizer, sequence, max_length,temprature=0.1,trust_remote_code=False):
         # ids = tokenizer.encode(f'{sequence}', return_tensors='pt')
+        if tokenizer.chat_template is None:
+            print("Warning: Chat template not found in tokenizer. Appling default chat template")
+            tokenizer.chat_template = """
+                {{ bos_token }}
+                {% for message in messages %}
+                    {% if message['role'] == 'system' %}
+                        {{ '<start_of_turn>system\n' + message['content'] | trim + '<end_of_turn>\n' }}
+                    {% elif message['role'] == 'user' %}
+                        {{ '<start_of_turn>user\n' + message['content'] | trim + '<end_of_turn>\n' }}
+                    {% elif message['role'] == 'assistant' %}
+                        {{ '<start_of_turn>assistant\n' + message['content'] | trim + '<end_of_turn>\n' }}
+                    {% else %}
+                        {{ raise_exception('Unsupported role: ' + message['role']) }}
+                    {% endif %}
+                {% endfor %}
+                {% if add_generation_prompt %}
+                    {{ '<start_of_turn>assistant\n' }}
+                {% endif %}
+                """
         formatted_chat = tokenizer.apply_chat_template(sequence, tokenize=False, add_generation_prompt=True)
         inputs = tokenizer(formatted_chat, return_tensors="pt", add_special_tokens=False)
         inputs = {key: tensor.to(model.device) for key, tensor in inputs.items()}
