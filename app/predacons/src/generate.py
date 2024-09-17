61482592e760fa18c6f23a45ed37d8c5cf5caa9a
Shourya Shashank <shouryashashank@gmail.com>
2024-09-17 21:12:59 +0530

    added auto quantize to all the model methods

--- app/predacons/src/generate.py
+++ app/predacons/src/generate.py
@@ -1,13 +1,44 @@
-from transformers import AutoModelForPreTraining, AutoTokenizer,AutoModelForCausalLM
+from transformers import AutoModelForPreTraining, AutoTokenizer,AutoModelForCausalLM,BitsAndBytesConfig
+import torch
+
 
 class Generate:
     default_chat_template = "{{ bos_token }}{% for message in messages %}{% if message['role'] == 'system' %}{{ '<start_of_turn>system\n' + message['content'] | trim + '<end_of_turn>\n' }}{% elif message['role'] == 'user' %}{{ '<start_of_turn>user\n' + message['content'] | trim + '<end_of_turn>\n' }}{% elif message['role'] == 'assistant' %}{{ '<start_of_turn>assistant\n' + message['content'] | trim + '<end_of_turn>\n' }}{% else %}{{ raise_exception('Unsupported role: ' + message['role']) }}{% endif %}{% endfor %}{% if add_generation_prompt %}{{ '<start_of_turn>assistant\n' }}{% endif %}"
-    def __load_model(model_path, trust_remote_code=False,gguf_file=None):
+    
+    def __load_model(model_path, trust_remote_code=False,gguf_file=None,auto_quantize=None):
         model = None
+        quantization_config = None
         try:
-            model = AutoModelForPreTraining.from_pretrained(model_path,trust_remote_code=trust_remote_code,gguf_file=gguf_file)
+            if auto_quantize.lower() == "4bit" or auto_quantize.lower() == "high":
+                print("Using 4bit quantization...")
+                quantization_config = BitsAndBytesConfig(
+                    load_in_4bit=True,
+                    bnb_4bit_quant_type="nf4",
+                    bnb_4bit_compute_dtype=torch.bfloat16
+                )
+            elif  auto_quantize.lower() == "8bit" or auto_quantize.lower() == "low":
+                print("Using 8bit quantization...")
+                quantization_config = BitsAndBytesConfig(
+                    load_in_8bit =True,
+                    bnb_4bit_compute_dtype=torch.bfloat16
+                )
+            else:
+                print("Error: Either give 4bit/high or 8bit/low as the value of auto_quantize. moving ahead without any quantization" )
+                quantization_config = None
         except:
-            model = AutoModelForCausalLM.from_pretrained(model_path,trust_remote_code=trust_remote_code,gguf_file=gguf_file)
+            quantization_config = None
+        if quantization_config is not None:
+            print("Quantization config: ",quantization_config)
+            try:
+                model = AutoModelForPreTraining.from_pretrained(model_path,trust_remote_code=trust_remote_code,gguf_file=gguf_file,quantization_config = quantization_config)
+            except:
+                model = AutoModelForCausalLM.from_pretrained(model_path,trust_remote_code=trust_remote_code,gguf_file=gguf_file,quantization_config = quantization_config)
+        else:
+            try:
+                model = AutoModelForPreTraining.from_pretrained(model_path,trust_remote_code=trust_remote_code,gguf_file=gguf_file)
+            except:
+                model = AutoModelForCausalLM.from_pretrained(model_path,trust_remote_code=trust_remote_code,gguf_file=gguf_file)
+        
 
         return model
 
@@ -16,8 +47,8 @@ class Generate:
         tokenizer = AutoTokenizer.from_pretrained(tokenizer_path,gguf_file=gguf_file)
         return tokenizer
     
-    def __generate_output(model_path, sequence, max_length,trust_remote_code=False,gguf_file=None):
-        model = Generate.__load_model(model_path,trust_remote_code=trust_remote_code,gguf_file=gguf_file)
+    def __generate_output(model_path, sequence, max_length,trust_remote_code=False,gguf_file=None,auto_quantize=None):
+        model = Generate.__load_model(model_path,trust_remote_code=trust_remote_code,gguf_file=gguf_file,auto_quantize=auto_quantize)
         tokenizer = Generate.__load_tokenizer(model_path,gguf_file=gguf_file)
         ids = tokenizer.encode(f'{sequence}', return_tensors='pt')
         final_outputs = model.generate(
@@ -30,8 +61,8 @@ class Generate:
         )
         return final_outputs,tokenizer
     
-    def __generate_chat_output(model_path, sequence, max_length,temprature = 0.1,trust_remote_code=False,gguf_file=None):
-        model = Generate.__load_model(model_path,trust_remote_code=trust_remote_code,gguf_file=gguf_file)
+    def __generate_chat_output(model_path, sequence, max_length,temprature = 0.1,trust_remote_code=False,gguf_file=None,auto_quantize = None):
+        model = Generate.__load_model(model_path,trust_remote_code=trust_remote_code,gguf_file=gguf_file,auto_quantize=auto_quantize)
         tokenizer = Generate.__load_tokenizer(model_path,gguf_file=gguf_file)
         if tokenizer.chat_template is None:
             print("Warning: Chat template not found in tokenizer. Appling default chat template")
@@ -45,8 +76,8 @@ class Generate:
             temperature=temprature)
         return inputs,final_outputs,tokenizer
     
-    def __generate_text(model_path, sequence, max_length,trust_remote_code=False,gguf_file=None):
-        final_outputs,tokenizer = Generate.__generate_output(model_path, sequence, max_length,trust_remote_code=trust_remote_code,gguf_file=gguf_file)
+    def __generate_text(model_path, sequence, max_length,trust_remote_code=False,gguf_file=None,auto_quantize=None):
+        final_outputs,tokenizer = Generate.__generate_output(model_path, sequence, max_length,trust_remote_code=trust_remote_code,gguf_file=gguf_file,auto_quantize=auto_quantize)
         
         print(tokenizer.decode(final_outputs[0], skip_special_tokens=True))
         return (tokenizer.decode(final_outputs[0], skip_special_tokens=True))
@@ -77,8 +108,8 @@ class Generate:
             temperature=temprature)
         return inputs,final_outputs,tokenizer
 
-    def generate_output(model_path, sequence, max_length,trust_remote_code=False,gguf_file=None):
-        return Generate.__generate_output(model_path, sequence, max_length,trust_remote_code=trust_remote_code,gguf_file=gguf_file)
+    def generate_output(model_path, sequence, max_length,trust_remote_code=False,gguf_file=None,auto_quantize=None):
+        return Generate.__generate_output(model_path, sequence, max_length,trust_remote_code=trust_remote_code,gguf_file=gguf_file,auto_quantize=auto_quantize)
     
     def generate_text(model_path, sequence, max_length,trust_remote_code=False,gguf_file=None):
         return Generate.__generate_text(model_path, sequence, max_length,trust_remote_code=trust_remote_code,gguf_file=gguf_file)
@@ -86,14 +117,14 @@ class Generate:
     def load_tokenizer(tokenizer_path,gguf_file=None):
         return Generate.__load_tokenizer(tokenizer_path,gguf_file=gguf_file)
     
-    def load_model(model_path,trust_remote_code=False,gguf_file = None):
-        return Generate.__load_model(model_path,trust_remote_code=trust_remote_code,gguf_file=gguf_file)
+    def load_model(model_path,trust_remote_code=False,gguf_file = None,auto_quantize=None):
+        return Generate.__load_model(model_path,trust_remote_code=trust_remote_code,gguf_file=gguf_file,auto_quantize=auto_quantize)
     
     def generate_output_from_model(model, tokenizer, sequence, max_length,trust_remote_code=False):
         return Generate.__generate_output_from_model(model, tokenizer, sequence, max_length,trust_remote_code=trust_remote_code)
     
-    def generate_chat_output(model_path, sequence, max_length,temprature = 0.1,trust_remote_code=False,gguf_file = None):
-        return Generate.__generate_chat_output(model_path, sequence, max_length,temprature = temprature,trust_remote_code=trust_remote_code,gguf_file=gguf_file)
+    def generate_chat_output(model_path, sequence, max_length,temprature = 0.1,trust_remote_code=False,gguf_file = None,auto_quantize=None):
+        return Generate.__generate_chat_output(model_path, sequence, max_length,temprature = temprature,trust_remote_code=trust_remote_code,gguf_file=gguf_file,auto_quantize=auto_quantize)
     
     def generate_chat_output_from_model(model, tokenizer, sequence, max_length,temprature=0.1,trust_remote_code=False):
         return Generate.__generate_chat_output_from_model(model, tokenizer, sequence, max_length,temprature=temprature,trust_remote_code=trust_remote_code)
