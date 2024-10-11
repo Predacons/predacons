5dd390eb08279e2022cf396c40e5d445a94351e6
Shourya Shashank <shouryashashank@gmail.com>
2024-10-12 00:01:05 +0530

    fixes

--- app/predacons/src/embeddings.py
+++ app/predacons/src/embeddings.py
@@ -9,12 +9,9 @@ class PredaconsEmbedding():
     def __init__(self, model_name: str = "sentence-transformers/paraphrase-MiniLM-L6-v2"):
         self.model = AutoModel.from_pretrained(model_name)
         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.model.to(self.device)
-        self.model.eval()
     
     #Mean Pooling - Take attention mask into account for correct averaging
-    def mean_pooling(model_output, attention_mask):
+    def mean_pooling(self,model_output, attention_mask):
         token_embeddings = model_output[0] #First element of model_output contains all token embeddings
         input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
         return torch.sum(token_embeddings * input_mask_expanded, 1) / torch.clamp(input_mask_expanded.sum(1), min=1e-9)
